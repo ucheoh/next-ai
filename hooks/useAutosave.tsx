5fca4ffc4f1dd79d7c8fa5f9e464aaf39834d4e1
UO <13673574-u_oo@users.noreply.gitlab.com>
2023-08-09 17:31:43 -0700

    Shifted custom hook files and props.

---
+++ hooks/useAutosave.tsx
@@ -0,0 +1,41 @@
+import { useRef, useEffect } from 'react';
+import { CommonProps } from '../types/props';
+import useDebounce from '@/hooks/useDebounce';
+
+export default function useAutosave<TData, TReturn>({
+  data,
+  onSave,
+  interval = 2000,
+  saveOnUnmount = true,
+}: CommonProps<TData, TReturn>) {
+  const valueOnCleanup = useRef(data);
+  const initialRender = useRef(true);
+  const handleSave = useRef(onSave);
+
+  const debouncedValueToSave = useDebounce(data, interval);
+
+  useEffect(() => {
+    if (initialRender.current) {
+      initialRender.current = false;
+    } else {
+      handleSave.current(debouncedValueToSave);
+    }
+  }, [debouncedValueToSave]);
+
+  useEffect(() => {
+    valueOnCleanup.current = data;
+  }, [data]);
+
+  useEffect(() => {
+    handleSave.current = onSave;
+  }, [onSave]);
+
+  useEffect(
+    () => () => {
+      if (saveOnUnmount) {
+        handleSave.current(valueOnCleanup.current);
+      }
+    },
+    [saveOnUnmount],
+  );
+}
