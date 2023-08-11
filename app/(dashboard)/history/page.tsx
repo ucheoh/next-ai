5f6d9062c7e432a110845b88d8e65b84ace47285
UO <13673574-u_oo@users.noreply.gitlab.com>
2023-08-10 23:40:51 -0700

    Added type for acc var in history

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,42 +1,45 @@
 import getUserByClerkID from "@/utils/auth";
 import prisma from "@/utils/db";
 import HistoryChart from "@/components/HistoryChart";
+import { Analysis } from "@/types/journal";
 
 const getData = async () => {
-  const user = await getUserByClerkID()
+  const user = await getUserByClerkID();
   const analyses = await prisma.analysis.findMany({
     where: {
       userId: user.id,
     },
     orderBy: {
-      createdAt: 'asc',
+      createdAt: "asc",
     },
-  })
+  });
 
-  const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
-  const avg = Math.round(sum / analyses.length)
-  return { analyses, avg }
-}
+  const sum = analyses.reduce(
+    (all: number, current) => all + current.sentimentScore,
+    0
+  );
+  const avg = Math.round(sum / analyses.length);
+  return { analyses, avg };
+};
 
 const fullScreenStyle = {
-  height: '85vh',
-  width: '100vw',
+  height: "85vh",
+  width: "100vw",
   margin: 0,
   paddingBottom: "80px",
   paddingLeft: "32px",
   paddingRight: "32px",
-  overflow: 'hidden'
+  overflow: "hidden",
 };
 
-export default async function History(){
-  const { avg, analyses } = await getData()
+export default async function History() {
+  const { avg, analyses } = await getData();
   return (
     <div className="w-full h-full">
-      <div style={{paddingLeft: "32px"}}>{`Avg. Sentiment ${avg}`}</div>
+      <div style={{ paddingLeft: "32px" }}>{`Avg. Sentiment ${avg}`}</div>
       <div style={fullScreenStyle}>
         <HistoryChart data={analyses} />
       </div>
     </div>
-  )
+  );
 }
-
